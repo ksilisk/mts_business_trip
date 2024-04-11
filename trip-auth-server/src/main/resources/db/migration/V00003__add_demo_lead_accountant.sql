92314a4766cba702203cc1be13fd8a445d7c7b19
Shaliko Salimov <shaliko9@yandex.ru>
2024-04-11 22:56:35 +0300

    feat: update migrations

---
+++ trip-auth-server/src/main/resources/db/migration/V00003__add_demo_lead_accountant.sql
@@ -0,0 +1,4 @@
+insert into users (username, password)
+VALUES ('lead', '$2a$10$UtmD2wPzokQv.y7FpldhQejR87hqbqHJeRP94iilZNi8qjRhWFKCO');
+insert into users (username, password)
+VALUES ('acco', '$2a$10$UtmD2wPzokQv.y7FpldhQejR87hqbqHJeRP94iilZNi8qjRhWFKCO')
